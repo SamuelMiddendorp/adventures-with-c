5bdca69f870b17b4cbac51f63d865a7244842598
Samuel Middendorp <samiezaurus@gmail.com>
2023-10-07 17:01:48 +0200

    Simply memoory arena

--- day2/main.c
+++ day2/main.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 
 #define SIZE 1
+#define ARENA_SIZE 10000
 
-void arena(){
+char arena[ARENA_SIZE] = {0};
+size_t arena_size = 0;
+
+void* arena_alloc(size_t size){
+    void* adress = ((char *)arena) + arena_size;
+    arena_size += size;
+    return adress;
+}
+void arena_func(){
     printf("Memory arena");
+    char* addr = arena_alloc(32);
+    for(int i = 0; i < 32; i++){
+        addr[i] = i + '0';
+        printf("value: %c \n", addr[i]);
+    }
 }
 
 int main(){
@@ -14,6 +28,6 @@ int main(){
         printf("character: %c \n", chars[i]);
         printf("adress: %p \n", &chars[i]);
     }
-    arena();
+    arena_func();
     return 0;
 }
