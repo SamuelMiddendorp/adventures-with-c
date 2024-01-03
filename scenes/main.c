9c397fd0dbfc18365381f6f7eb88e8c6ab100da9
Samuel Middendorp <samiezaurus@gmail.com>
2024-01-03 23:17:56 +0100

    Add scene system

--- scenes/main.c
+++ scenes/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define MAX_SCENES 100;
-
+#define MAX_SCENES 100
 
 typedef struct{
 	void(*run)();
@@ -10,10 +10,47 @@ typedef struct{
 typedef struct{
 	Scene** stateStack;
 	int current;
-};
+} SceneManager;
+
+void run1(){
+	printf("BaDaBing \n");
+}
+
+void run2(){
+	printf("BaDaBoom \n");
+}
+
+void initSceneManager(SceneManager* sceneManager){
+	sceneManager->stateStack = malloc(MAX_SCENES * sizeof(Scene*));
+	sceneManager->current = 0;
+}
+
+void pushScene(SceneManager* sceneManager, Scene* scene){
+	sceneManager->stateStack[++sceneManager->current] = scene;	
+}
+
+void runScene(SceneManager* sceneManager){
+	sceneManager->stateStack[sceneManager->current]->run();
+}
 
 
 int main(){
-	printf("foobar");
+	printf("foobar \n");
+
+	SceneManager sceneManager;
+	initSceneManager(&sceneManager);
+
+	Scene s1 = {&run1};
+	Scene s2 = {&run2};
+
+	pushScene(&sceneManager, &s1);
+	runScene(&sceneManager);
+
+	pushScene(&sceneManager, &s2);
+	runScene(&sceneManager);
+
+	pushScene(&sceneManager, &s1);
+	runScene(&sceneManager);
+
 	return 0;
 }
